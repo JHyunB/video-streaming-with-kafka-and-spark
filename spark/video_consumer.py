c07d12ee2dc66685d280e58e9a09a3b9e8a8253d
JHyunB <whdgus4892@gmail.com>
2020-10-07 16:25:15 +0900

    fix typo

--- spark/video_consumer.py
+++ spark/video_consumer.py
@@ -2,24 +2,23 @@ import sys
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-from confluent_kafka.avro.serializer.message_serializer import MessageSerializer
 
 import cv2
 import numpy as np
 
 
 def deserializer(img):
-    return np.frombuffer(img, dtype=np.unit8)
+    return img[0], np.frombuffer(img[1], dtype=np.uint8)
 
 
 def decode(img):
-    return cv2.imdecode(img, cv2.IMREAD_COLOR)
+    return img[0], cv2.imdecode(img[1], cv2.IMREAD_COLOR)
 
 
 sc = SparkContext(appName="test")
 ssc = StreamingContext(sc,1)
 brokers, topic = sys.argv[1:]
-kafka_stream = KafkaUtils.createDirectStream(ssc,[topic],{"metadata.broker.list":brokers}, valueDecoder=MessageSerializer.decode_message)
+kafka_stream = KafkaUtils.createDirectStream(ssc,[topic],{"metadata.broker.list":brokers}, valueDecoder=lambda x: x)
 frame = kafka_stream.map(deserializer).map(decode)
 frame.pprint()
 ssc.start()
