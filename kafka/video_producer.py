879805a27396834353d32f11eb529b6cbf0d390c
JHyunB <whdgus4892@gmail.com>
2020-10-07 12:57:29 +0900

    Add kafka sample code

---
+++ kafka/video_producer.py
@@ -0,0 +1,18 @@
+from kafka import KafkaProducer
+import cv2
+
+topic = "testing"
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+video_file = "test_data/Robbery056_x264.mp4"
+video = cv2.VideoCapture(video_file)
+
+while video.isOpened():
+    success, frame = video.read()
+    if not success:
+        print("read failed!!")
+        break
+    _, img = cv2.imencode('.jpg',frame)
+    producer.send(topic, img.tobytes())
+
+video.release()
