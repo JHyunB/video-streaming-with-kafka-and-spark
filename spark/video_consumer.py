e9fab80eb33fc3c586b4182c676b88abdbd0ed60
JHyunB <whdgus4892@gmail.com>
2020-10-08 17:43:52 +0900

    fix

--- spark/video_consumer.py
+++ spark/video_consumer.py
@@ -1,12 +1,7 @@
 import sys
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-<<<<<<< HEAD
-from pyspark.streaming.context import KafkaUtils
-from confluent_kafka.avro.serializer.message_serializer import MessageSerializer
-=======
 from pyspark.streaming.kafka import KafkaUtils
->>>>>>> c07d12ee2dc66685d280e58e9a09a3b9e8a8253d
 
 import cv2
 import numpy as np
@@ -18,7 +13,6 @@ from spark.utils.array_util import sliding_window
 
 
 def deserializer(img):
-<<<<<<< HEAD
     return img[0], np.frombuffer(img[1], dtype=np.unit8)
 
 
@@ -33,33 +27,15 @@ def sliding_window(arr, size, stride):
         if len(arr[i:i + size]) > 0:
             result.append(arr[i:i + size])
     return np.array(result)
-=======
-    return img[0], np.frombuffer(img[1], dtype=np.uint8)
-
-
-def decode(img):
-    return img[0], cv2.imdecode(img[1], cv2.IMREAD_COLOR)
->>>>>>> c07d12ee2dc66685d280e58e9a09a3b9e8a8253d
 
 
 sc = SparkContext(appName="test")
 ssc = StreamingContext(sc,1)
 brokers, topic = sys.argv[1:]
-<<<<<<< HEAD
 kafka_stream = KafkaUtils.createDirectStream(ssc,[topic],{"metadata.broker.list":brokers}, valueDecoder=MessageSerializer.decode_message)
 frames = kafka_stream.map(deserializer).map(decode).map(lambda x: x[1])
 clips = sliding_window(frames, params.frame_count, params.frame_count)
-'''
-데이터 전처리
-딥러닝 분류
-'''
 
-
-frames.pprint()
-=======
-kafka_stream = KafkaUtils.createDirectStream(ssc,[topic],{"metadata.broker.list":brokers}, valueDecoder=lambda x: x)
-frame = kafka_stream.map(deserializer).map(decode)
-frame.pprint()
->>>>>>> c07d12ee2dc66685d280e58e9a09a3b9e8a8253d
+clips.pprint()
 ssc.start()
 ssc.awaitTermination()
