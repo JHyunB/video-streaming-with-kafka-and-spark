82d2672c3b88bd5ac4868a8594ec2ed59a3c33f8
JHyunB <whdgus4892@gmail.com>
2020-11-27 20:29:16 +0900

    fix

--- spark/utils/array_util.py
+++ spark/utils/array_util.py
@@ -1,15 +1,6 @@
 import numpy as np
 
 
-def sliding_window(arr, size, stride):
-    num_chunks = int((len(arr) - size) / stride) + 2
-    result = []
-    for i in range(0, num_chunks * stride, stride):
-        if len(arr[i:i + size]) > 0:
-            result.append(arr[i:i + size])
-    return np.array(result)
-
-
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
@@ -50,17 +41,3 @@ def extrapolate(outputs, num_frames):
     for index in extrapolation_indicies:
         extrapolated_outputs.append(outputs[int(index)])
     return np.array(extrapolated_outputs)
-
-
-def test_interpolate():
-    test_case1 = np.random.randn(24, 2048)
-    output_case1 = interpolate(test_case1, 32)
-    assert output_case1.shape == (32, 2048)
-
-    test_case2 = np.random.randn(32, 2048)
-    output_case2 = interpolate(test_case2, 32)
-    assert output_case2.shape == (32, 2048)
-
-    test_case3 = np.random.randn(42, 2048)
-    output_case3 = interpolate(test_case3, 32)
-    assert output_case3.shape == (32, 2048)
